a95fce155bb35e8c071c6d523225a9e62374f6bd
wujinli <wujinli>
2018-03-20 18:21:56 +0800

    持续化存储——DB

--- app/src/main/java/com/wjl/reviewdemo/storage/DBHelper.java
+++ app/src/main/java/com/wjl/reviewdemo/storage/DBHelper.java
@@ -1,7 +1,6 @@
 package com.wjl.reviewdemo.storage;
 
 import android.content.Context;
-import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
@@ -13,8 +12,12 @@ import android.widget.Toast;
  */
 
 public class DBHelper extends SQLiteOpenHelper {
+    //创建表Book的SQL语句
     public static final String CREATE_BOOK = "create table Book(id integer primary key autoincrement,author text,price real,pages integer,name text)";
+    //创建表Categroy的SQL语句
+    public static final String CREATE_CATEGROY = "create table Categroy(id integer primary key autoincrement,categroy_name text,categroy_code integer)";
     public Context mContext;
+
 
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -23,12 +26,26 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //创建表的操作
         sqLiteDatabase.execSQL(CREATE_BOOK);
+        sqLiteDatabase.execSQL(CREATE_CATEGROY);
         Toast.makeText(mContext, "create table successful", Toast.LENGTH_SHORT).show();
     }
 
+
+    /**
+     * 调用此方法的前提是，在初始化Helper是构造方法中version大于之前的vesion
+     *
+     * @param sqLiteDatabase
+     * @param i
+     * @param i1
+     */
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        //更新数据库，在数据库与存在下，将之前的删除
+        sqLiteDatabase.execSQL("drop table if exists Book");
+        sqLiteDatabase.execSQL("drop table if exists Categroy");
 
+        onCreate(sqLiteDatabase);
     }
 }
--- app/src/main/java/com/wjl/reviewdemo/storage/DataBaseActivity.java
+++ app/src/main/java/com/wjl/reviewdemo/storage/DataBaseActivity.java
@@ -1,9 +1,14 @@
 package com.wjl.reviewdemo.storage;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.wjl.reviewdemo.R;
 import com.wjl.reviewdemo.base.BaseActivity;
@@ -17,24 +22,123 @@ import com.wjl.reviewdemo.base.BaseActivity;
 public class DataBaseActivity extends BaseActivity implements View.OnClickListener {
     DBHelper dbHelper;
 
-    Button btn_creat_db;
+    Button btn_creat_db, btn_add, btn_update, btn_select, btn_delete;
+    SQLiteDatabase sqLiteDatabase;
+    int id = 0;
+
+    EditText et_author, et_price, et_pages, et_name;
+
+    String author, name;
+    int pages;
+    float price;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_db);
         dbHelper = new DBHelper(this, "BookStore.db", null, 1);
-
-        btn_creat_db=findViewById(R.id.btn_creat_db);
+        sqLiteDatabase = dbHelper.getWritableDatabase();
+        btn_creat_db = findViewById(R.id.btn_creat_db);
+        btn_add = findViewById(R.id.btn_add);
+        btn_update = findViewById(R.id.btn_update);
+        btn_select = findViewById(R.id.btn_select);
+        btn_delete = findViewById(R.id.btn_delete);
+        et_author = findViewById(R.id.et_author);
+        et_price = findViewById(R.id.et_price);
+        et_pages = findViewById(R.id.et_pages);
+        et_name = findViewById(R.id.et_name);
 
         btn_creat_db.setOnClickListener(this);
+        btn_add.setOnClickListener(this);
+        btn_update.setOnClickListener(this);
+        btn_select.setOnClickListener(this);
+        btn_delete.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_creat_db:
-                dbHelper.getWritableDatabase();
+
+                break;
+            case R.id.btn_add:
+                insertData();
+                break;
+            case R.id.btn_update:
+                updateData();
+                break;
+            case R.id.btn_select:
+                break;
+            case R.id.btn_delete:
+                break;
+            default:
                 break;
         }
+    }
+
+
+    public void getEditeTextValues() {
+        author = et_author.getText().toString();
+
+        if (et_price.getText() == null) {
+            price = 0;
+        } else if (TextUtils.isEmpty(et_price.getText().toString())) {
+            price = 0;
+        } else {
+            price = Float.parseFloat(et_price.getText().toString());
+        }
+
+        if (et_pages.getText() == null) {
+            pages = 0;
+        } else if (TextUtils.isEmpty(et_pages.getText().toString())) {
+            pages = 0;
+        } else {
+            pages = Integer.parseInt(et_pages.getText().toString());
+        }
+        pages = Integer.parseInt(et_pages.getText().toString());
+        name = et_name.getText().toString();
+    }
+
+
+    /**
+     * 添加数据
+     */
+    public void insertData() {
+        getEditeTextValues();
+        ContentValues values = new ContentValues();
+
+        values.put("id", id);
+        values.put("author", author);
+        values.put("price", price);
+        values.put("pages", pages);
+        values.put("name", name);
+        sqLiteDatabase.insert("Book", null, values);
+        id++;
+        values.clear();
+
+        Toast.makeText(this, "添加完成", Toast.LENGTH_SHORT).show();
+    }
+
+
+    /**
+     * 更新数据
+     */
+    public void updateData() {
+        getEditeTextValues();
+
+        ContentValues values = new ContentValues();
+        values.put("author", author);
+        values.put("price", price);
+        values.put("pages", pages);
+        values.put("name", name);
+        sqLiteDatabase.update("Book", values, "name=?", new String[]{"haha"});
+        values.clear();
+    }
+
+    /**
+     * 删除数据
+     */
+    public void deleteData() {
+        sqLiteDatabase.delete("Book", "name=?", new String[]{"haha"});
     }
 }
